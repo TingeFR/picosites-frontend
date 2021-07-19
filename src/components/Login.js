34c74f6a92eadb8e208f2ae1a8dd73b874281665
qpicault <qpicault@yahoo.com>
2021-07-20 00:45:19 +0200

    menus

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive'
-import { connect } from 'react-redux'
 import { constants } from '../assets/utils'
+import { connect } from 'react-redux'
 import { Button, Card, Elevation, InputGroup } from "@blueprintjs/core";
-import { Colors } from "@blueprintjs/core";
+import { Text, Colors } from "@blueprintjs/core";
 import PicoSitesLogo from "../assets/svg/PicoSitesLogo"
 
 function Login(props) {
@@ -20,9 +20,11 @@ function Login(props) {
       overflow: "auto",
     },
     loginCard: {
+      display: "flex",
+      flexDirection: "column",
       marginTop: isDesktop ? 56 : 0,
       width: isDesktop ? 460 : "100%",
-      height: 700,
+      height: 560,
     },
     loginLogoContainer: {
       width: "100%",
@@ -41,6 +43,11 @@ function Login(props) {
       height: 48,
       padding: 24,
       marginTop: 16
+    },
+    loginFooter: {
+      width: "100%",
+      height: 30,
+      textAlign: "center",
     }
   }
 
@@ -56,9 +63,11 @@ function Login(props) {
         <div id="loginLogoContainer" style={styles.loginLogoContainer}>
           <PicoSitesLogo style={styles.loginLogo}/>
         </div>
-        <InputGroup type="email" placeholder="Email" style={styles.loginFormElement}/>
-        <InputGroup type="password" placeholder="Password" style={styles.loginFormElement}/>
+        <InputGroup type="email" placeholder={i18n.email} style={styles.loginFormElement}/>
+        <InputGroup type="password" placeholder={i18n.password} style={styles.loginFormElement}/>
         <Button minimal fill className="picoButton" style={styles.loginFormElement}>{i18n.login}</Button>
+        <div style={{flexGrow: 1}}></div>
+        <Text id="loginFooter" style={styles.loginFooter}>©2021 PicoSites</Text>
       </Card>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive'
+import { constants } from '../assets/utils'
 import { connect } from 'react-redux'
 import Login from "./Login";
 import { Redirect, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFound from './NotFound';
-import { Menu, MenuItem } from '@blueprintjs/core';
+import { Menu, MenuDivider, MenuItem } from '@blueprintjs/core';
+import PicoSitesLogo from '../assets/svg/PicoSitesLogo';
 
 function RedirectToHome() {
   return(
@@ -13,6 +16,8 @@ function RedirectToHome() {
 
 function Main(props) {
 
+  const isDesktop = useMediaQuery({ minWidth: constants.DESKTOP_TO_MOBILE + 1 })
+
   const styles = {
     mainContainer: {
       position: "fixed",
@@ -20,12 +25,32 @@ function Main(props) {
       height: "100%",
       left: 0,
       top: 0
-    }
+    },
+    mainSidebar: {
+      position: "absolute",
+      width: isDesktop ? 256 : "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      bottom: 0,
+      background: "white",
+      borderRight: "1px solid rgba(24, 32, 38, 0.2)"
+    },
+    mainSideLogo: {
+      padding: 24,
+      paddingLeft: 20,
+    },
   }
 
+  const [i18n, seti18n] = useState({})
+
   useEffect(() => {
-    _i18nSetLanguage("en")
+    _i18nSetLanguage("fr")
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    seti18n(props.i18n)
+  }, [props.i18n])
 
   const _i18nSetLanguage = (lang) => {
     const action = { type: "I18N_SET_LANGUAGE", value: lang }
@@ -35,9 +60,17 @@ function Main(props) {
   return (
     <div id="mainContainer" style={styles.mainContainer}>
       <Router style={styles.router}>
-        {/* <Menu>
-          <MenuItem icon="new-link" text="New link" />
-        </Menu> */}
+        <div id="mainSidebar" style={styles.mainSidebar}>
+          <Menu className="picoMenu" large>
+            <div id="mainSideLogo" style={styles.mainSideLogo}><PicoSitesLogo/></div>
+            <MenuItem className="picoMenuItem" icon="home" text={i18n.overview}/>
+            <MenuDivider className="picoMenuDivider" title={i18n.projects}/>
+            <MenuItem className="picoMenuItem" text="Projet 1"/>
+            <MenuDivider/>
+            <MenuItem className="picoMenuItem" icon="chat" text={i18n.requests}/>
+            <MenuItem className="picoMenuItem" icon="edit" text={i18n.fields}/>
+          </Menu>
+        </div>
         <Switch>
           <Route exact path="/" component={props.isLoggedIn ? "" : Login} />
           <Route component={NotFound}/>
@@ -49,7 +82,8 @@ function Main(props) {
 
 const mapStateToProps = (state) => {
   return {
-    isLoggedIn: state.isLoggedIn
+    isLoggedIn: state.isLoggedIn,
+    i18n: state.i18n
   }
 }
 
